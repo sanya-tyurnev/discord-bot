7899105d8a8bf2d969637bc6770eeb054eba5cf8
sanya-tyurnev <55585012+sanya-tyurnev@users.noreply.github.com>
2019-09-21 13:12:57 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,32 +1,13 @@
 import discord
-import asyncio
-import requests
+from discord.ext import commands
 
-DISCORD_BOT_TOKEN = 'tTmRFg8NMIl4wlw5K0e8hniFUJqkT38_'
+TOKEN = 'tTmRFg8NMIl4wlw5K0e8hniFUJqkT38_'
+bot = commands.Bot(command_prefix='!')
 
-BTC_PRICE_URL_coinmarketcap = 'https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=RUB'
 
-client = discord.Client()
+@bot.command(pass_context=True)  # разрешаем передавать агрументы
+async def test(ctx, arg):  # создаем асинхронную фунцию бота
+    await ctx.send(arg)  # отправляем обратно аргумент
 
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
 
-@client.event
-async def on_message(message):
-    if message.content.startswith('!btcprice'):
-        print('[command]: btcprice ')
-        btc_price_usd, btc_price_rub = get_btc_price()
-        await client.send_message(message.channel, 'USD: ' + str(btc_price_usd) + ' | RUB: ' + str(btc_price_rub))
-
-def get_btc_price():
-    r = requests.get(BTC_PRICE_URL_coinmarketcap)
-    response_json = r.json()
-    usd_price = response_json[0]['price_usd']
-    rub_rpice = response_json[0]['price_rub']
-    return usd_price, rub_rpice
-
-client.run(DISCORD_BOT_TOKEN)
+bot.run(TOKEN)
