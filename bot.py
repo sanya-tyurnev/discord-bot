cb09aed16b1c0f5e58aa5afc87d56ecfbb32be56
sanya-tyurnev <55585012+sanya-tyurnev@users.noreply.github.com>
2019-09-22 14:51:31 +0800

    Add files via upload

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ async def on_message(message):
     if message.content.startswith('!стата'):
         print("Запрос статы")
         points, rank, league = get_statistics()
-        await message.channel.send("Очки: " + "{:,}".format(points).replace(',', ' ') + "\nЛига: " + league + "\nМесто: " + str(rank))
+        await message.channel.send("Очки: " + str("{:,}".format(points).replace(',', ' ')) + "\nЛига: " + league + "\nМесто: " + str(rank))
 
 def get_statistics():
     response = requests.get("http://raptus-statistics.000webhostapp.com/get.php?type=bot")
