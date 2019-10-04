d3fce3db8b28acda84be74ee3992212712c9609f
sanya-tyurnev <55585012+sanya-tyurnev@users.noreply.github.com>
2019-10-04 21:56:24 +0800

    Add files via upload

--- bot.py
+++ bot.py
@@ -80,10 +80,11 @@ async def clear(ctx):
             
             if moderator == str(ctx.author.id):
                 is_moderator = True
-                await ctx.channel.purge(limit=None, check=lambda msg: not msg.pinned)
                 
         if is_moderator == False:
             await ctx.send("У тебя нет здесь власти :unamused:")
+        else:
+            await ctx.channel.purge(limit=None, check=lambda msg: not msg.pinned)
                 
 @client.command()
 async def rm(ctx):
